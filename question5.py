c8be3ebce2af1999773177d161cc335c80caf207
UnknownXstark <nathanlouiskalio@gmail.com>
2025-08-10 21:07:47 +0100

    question 5 done

---
+++ question5.py
@@ -0,0 +1,13 @@
+#  Write a function to find the second largest number in a list without using sorting.
+
+def secondLargest(nums):
+    largest = secondLargest = float('-inf')
+    for n in nums:
+        if n > largest:
+            secondLargest = largest
+            largest = n
+        elif n > secondLargest and n!= largest:
+            secondLargest = n
+            return secondLargest if secondLargest != float('-inf') else None
+
+print(secondLargest([5,5,5,5]))
